664d2fc6b1209cb5460796cc6694b53ab1051931
Elpah <obengelpachris@gmail.com>
2024-11-04 04:38:41 +0100

    sidebar layout

--- frontend/src/components/sidebar-filters/SidebarFilters.tsx
+++ frontend/src/components/sidebar-filters/SidebarFilters.tsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React from "react";
 
-import styles from "./sidebar-filter.module.scss"
-import { Category } from './categories/Category'
+import styles from "./sidebar-filter.module.scss";
+import { Category } from "./categories/Category";
 export const SidebarFilters = () => {
   return (
     <div className={styles.container}>
-       <Category/>
-       <Category/>
-       <Category/>
-       <Category/>
+      <Category />
+      <Category />
+      <Category />
+      <Category />
+      <Category />
+      <Category />
+      <Category />
+      <Category />
+      <Category />
     </div>
-  )
-}
+  );
+};
--- frontend/src/components/sidebar-filters/categories/Category.tsx
+++ frontend/src/components/sidebar-filters/categories/Category.tsx
@@ -1,42 +1,56 @@
-import React from 'react'
-import plusIcon from "../../../assets/images/category_icons/plus_icon.png"
-import minusIcon from "../../../assets/images/category_icons/minus_icon.png"
+import React, { useState } from "react";
+import plusIcon from "../../../assets/images/category_icons/plus_icon.png";
+import minusIcon from "../../../assets/images/category_icons/minus_icon.png";
 
-
-import styles from "./category.module.scss"
-import { CategoryCheckItem } from '../category-check-item/CategoryCheckItem'
+import styles from "./category.module.scss";
+import { CategoryCheckItem } from "../category-check-item/CategoryCheckItem";
 export const Category = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-    
+  const toggleItems = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className={styles.container}>
-        <div className={styles.sub_wrapper}>
-        <div className={styles.header_container}>
-            <div className={styles.header}> Search By Brand</div>
-            <div className={styles.open_close_icon_container}>
-                <img className={styles.open_close_icon} src={plusIcon} alt="open or close" />
-            </div>
+      <div className={styles.sub_wrapper}>
+        <div className={styles.header_container} onClick={toggleItems}>
+          <div className={styles.header}> Search By Brand</div>
+          <div className={styles.open_close_icon_container}>
+            <img
+              className={styles.open_close_icon}
+              src={isOpen ? minusIcon : plusIcon}
+              alt="open or close"
+            />
+          </div>
         </div>
-        <div className={styles.cat_items_container}>
-            <CategoryCheckItem/>
-            <CategoryCheckItem/>
-            <CategoryCheckItem/>
-            <CategoryCheckItem/>
-            <CategoryCheckItem/>
-            <CategoryCheckItem/>
-            <CategoryCheckItem/>
-            <CategoryCheckItem/><CategoryCheckItem/>
-            <CategoryCheckItem/>
-            <CategoryCheckItem/>
-            <CategoryCheckItem/><CategoryCheckItem/>
-            <CategoryCheckItem/>
-            <CategoryCheckItem/>
-            <CategoryCheckItem/><CategoryCheckItem/>
-            <CategoryCheckItem/>
-            <CategoryCheckItem/>
-            <CategoryCheckItem/>
+        <div
+          className={`${styles.cat_items_container} ${
+            isOpen ? styles.open : ""
+          }`}
+        >
+          <CategoryCheckItem />
+          <CategoryCheckItem />
+          <CategoryCheckItem />
+          <CategoryCheckItem />
+          <CategoryCheckItem />
+          <CategoryCheckItem />
+          <CategoryCheckItem />
+          <CategoryCheckItem />
+          <CategoryCheckItem />
+          <CategoryCheckItem />
+          <CategoryCheckItem />
+          <CategoryCheckItem />
+          <CategoryCheckItem />
+          <CategoryCheckItem />
+          <CategoryCheckItem />
+          <CategoryCheckItem />
+          <CategoryCheckItem />
+          <CategoryCheckItem />
+          <CategoryCheckItem />
+          <CategoryCheckItem />
         </div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
