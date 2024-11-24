6a7506e6bb24325c36f82243cf97278c704a93c4
elpah <Obengelpachris@gmail.com>
2024-11-24 19:30:27 +0100

    changes to see if ssh now working

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,10 +8,10 @@ dotenv.config();
 const app = express();
 app.use(cors());
 
-const port = process.env.BACKEND_PORT || 80;
-console.log("PORT = ", port)
+const PORT = process.env.BACKEND_PORT || 80;
+
 app.use(express.json());
 app.use("/api/", carRouter);
 
-app.listen(port, '0.0.0.0', () => console.log(`listening on port ${port}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`listening on port ${PORT}`));
 export default app;
