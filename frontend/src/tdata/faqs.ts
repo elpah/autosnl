24b6d31da56b08b90d6920e16ac4c0edce49e0ea
Elpah <obengelpachris@gmail.com>
2025-01-02 04:25:55 +0100

    extract faq and whys into separate file

---
+++ frontend/src/tdata/faqs.ts
@@ -0,0 +1,62 @@
+export const faqs = [
+  {
+    id: 1,
+    title: "Is the prices of the cars Negotiable? ",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis nisl ut eros pulvinar tincidunt. Nulla facilisi. Sed eget arcu nec justo ultricies viverra. Proin non turpis non libero viverra feugiat. Mauris placerat, nisi ac fermentum aliquam, risus libero fermentum sapien, at posuere justo nisi et lacus. Fusce fermentum semper orci, ut tempus dui faucibus id.",
+  },
+  {
+    id: 2,
+    title: "Is the prices of the cars Negotiable? ",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis nisl ut eros pulvinar tincidunt. Nulla facilisi. Sed eget arcu nec justo ultricies viverra. Proin non turpis non libero viverra feugiat. Mauris placerat, nisi ac fermentum aliquam, risus libero fermentum sapien, at posuere justo nisi et lacus. Fusce fermentum semper orci, ut tempus dui faucibus id.",
+  },
+  {
+    id: 3,
+    title: "Is the prices of the cars Negotiable? ",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis nisl ut eros pulvinar tincidunt. Nulla facilisi. Sed eget arcu nec justo ultricies viverra. Proin non turpis non libero viverra feugiat. Mauris placerat, nisi ac fermentum aliquam, risus libero fermentum sapien, at posuere justo nisi et lacus. Fusce fermentum semper orci, ut tempus dui faucibus id.",
+  },
+  {
+    id: 4,
+    title: "Is the prices of the cars Negotiable? ",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis nisl ut eros pulvinar tincidunt. Nulla facilisi. Sed eget arcu nec justo ultricies viverra. Proin non turpis non libero viverra feugiat. Mauris placerat, nisi ac fermentum aliquam, risus libero fermentum sapien, at posuere justo nisi et lacus. Fusce fermentum semper orci, ut tempus dui faucibus id.",
+  },
+  {
+    id: 5,
+    title: "Is the prices of the cars Negotiable? ",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis nisl ut eros pulvinar tincidunt. Nulla facilisi. Sed eget arcu nec justo ultricies viverra. Proin non turpis non libero viverra feugiat. Mauris placerat, nisi ac fermentum aliquam, risus libero fermentum sapien, at posuere justo nisi et lacus. Fusce fermentum semper orci, ut tempus dui faucibus id.",
+  },
+  {
+    id: 6,
+    title: "Is the prices of the cars Negotiable? ",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis nisl ut eros pulvinar tincidunt. Nulla facilisi. Sed eget arcu nec justo ultricies viverra. Proin non turpis non libero viverra feugiat. Mauris placerat, nisi ac fermentum aliquam, risus libero fermentum sapien, at posuere justo nisi et lacus. Fusce fermentum semper orci, ut tempus dui faucibus id.",
+  },
+  {
+    id: 7,
+    title: "Is the prices of the cars Negotiable? ",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis nisl ut eros pulvinar tincidunt. Nulla facilisi. Sed eget arcu nec justo ultricies viverra. Proin non turpis non libero viverra feugiat. Mauris placerat, nisi ac fermentum aliquam, risus libero fermentum sapien, at posuere justo nisi et lacus. Fusce fermentum semper orci, ut tempus dui faucibus id.",
+  },
+  {
+    id: 8,
+    title: "Is the prices of the cars Negotiable? ",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis nisl ut eros pulvinar tincidunt. Nulla facilisi. Sed eget arcu nec justo ultricies viverra. Proin non turpis non libero viverra feugiat. Mauris placerat, nisi ac fermentum aliquam, risus libero fermentum sapien, at posuere justo nisi et lacus. Fusce fermentum semper orci, ut tempus dui faucibus id.",
+  },
+  {
+    id: 9,
+    title: "Is the prices of the cars Negotiable? ",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis nisl ut eros pulvinar tincidunt. Nulla facilisi. Sed eget arcu nec justo ultricies viverra. Proin non turpis non libero viverra feugiat. Mauris placerat, nisi ac fermentum aliquam, risus libero fermentum sapien, at posuere justo nisi et lacus. Fusce fermentum semper orci, ut tempus dui faucibus id.",
+  },
+  {
+    id: 10,
+    title: "Is the prices of the cars Negotiable? ",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis nisl ut eros pulvinar tincidunt. Nulla facilisi. Sed eget arcu nec justo ultricies viverra. Proin non turpis non libero viverra feugiat. Mauris placerat, nisi ac fermentum aliquam, risus libero fermentum sapien, at posuere justo nisi et lacus. Fusce fermentum semper orci, ut tempus dui faucibus id.",
+  },
+];
---
+++ frontend/src/tdata/whys.ts
@@ -0,0 +1,47 @@
+import {
+  quality_assurance,
+  useful_suggest,
+  best_rate,
+  variety_options,
+  great_offers,
+  match_requirement,
+} from "../assets/images/images";
+
+export const whys = [
+  {
+    title: "Quality Assurance",
+    paragraph:
+      "Our cars undergo rigorous inspections and come with detailed history reports. Drive with confidence knowing your vehicle meets the highest safety and performance standards.",
+    image: `${quality_assurance}`,
+  },
+  {
+    title: "Match Requirement",
+    paragraph:
+      "Our diverse inventory includes something for everyone. Easily find the perfect vehicle by filtering by make, model, year, price, and more.",
+    image: `${match_requirement}`,
+  },
+  {
+    title: "A variety of Options",
+    paragraph:
+      "Our extensive inventory includes a wide range of vehicles—from compact cars and sedans to SUVs and trucks. Find the perfect match for your needs and preferences with ease.",
+    image: `${variety_options}`,
+  },
+  {
+    title: "Useful Suggestions",
+    paragraph:
+      "Our diverse inventory includes something for everyone. Easily find the perfect vehicle by filtering by make, model, year, price, and more.",
+    image: `${useful_suggest}`,
+  },
+  {
+    title: "Best rate in the Market",
+    paragraph:
+      "We offer competitive pricing with no hidden fees. Enjoy the best deals and flexible financing options to make your dream car affordable.",
+    image: `${best_rate}`,
+  },
+  {
+    title: "Great Offers",
+    paragraph:
+      "Benefit from our exclusive deals and promotions. Stay updated with our latest offers to maximize your savings on your next car purchase.",
+    image: `${great_offers}`,
+  },
+];
