5ce6e4be8a46465dc231ee239d8c19921495537d
Elpah <obengelpachris@gmail.com>
2025-03-14 02:16:13 +0100

    setup query to get single car

---
+++ frontend/src/hooks/useGetCarById.ts
@@ -0,0 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { type ICarDataResponse} from "../types/carResponseType";
+import { IDealer } from "../types/otherTypes";
+
+type ICarByIdResponse = {
+  car: ICarDataResponse;
+  dealer: IDealer;
+}
+const useGetCarById = (carId: string) => {
+  const fetchCarById = () =>
+    axios
+      .get<ICarByIdResponse>("http://localhost:8080/api/carById", {
+        params: { carId },
+      })
+      .then((res) => res.data);
+
+  return useQuery<ICarByIdResponse, Error>({
+    queryKey: ["carById", carId],
+    queryFn: fetchCarById,
+    enabled: !!carId,
+  });
+};
+
+export default useGetCarById;
