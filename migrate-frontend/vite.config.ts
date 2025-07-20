bbc9b1d33616cac20818890182fa7e902e2f9b00
Elpah <obengelpachris@gmail.com>
2025-07-20 20:03:16 +0200

    fix build error

--- migrate-frontend/vite.config.ts
+++ migrate-frontend/vite.config.ts
@@ -1,18 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Use `import.meta.resolve` instead of `require.resolve` if you're working with ES Modules
-import path from "path";
-
+// https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-  resolve: {
-    alias: {
-      // Polyfill for crypto module
-      crypto: path.resolve(__dirname, "node_modules/crypto-browserify"),
-    },
-  },
-  build: {
-    chunkSizeWarningLimit: 500,
-  },
 });
